a19da84ae198940835fbf31e2eb5e6e99ff19b2a
zantaclaus <tarzuto@gmail.com>
2022-11-09 21:45:01 +0700

    Do everything

---
+++ behavioral-design-patterns/behavioral/NameSortStrategy.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class NameSortStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people) {
+        // YOU ARE NOT ALLOWED TO MODIFY THE CODE ABOVE THIS COMMENT !!
+        // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY
+        // CONTAIN UP TO ONE SEMI-COLON !!
+        people.sort(new Comparator<Person>() {
+            public int compare(Person a, Person b) {
+                return a.getName().compareTo(b.getName());
+            }
+        });
+    }
+}
